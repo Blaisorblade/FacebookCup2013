1cd688ae560fa6fc88d00cc0e9dce7f3137c8554
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-01-28 05:13:55 +0100

    FindTheMin WIP
    RNG generation

--- src/main/scala/FindTheMin.scala
+++ src/main/scala/FindTheMin.scala
@@ -1,7 +1,32 @@
 import java.lang.Long
 import language.postfixOps
+import scala.collection.IndexedSeqLike
+import scala.collection.mutable.ArrayBuffer
 
-object FindTheMin extends CmdlineInput {
+/*
+class RingBuffer[T](buf: Array[T], override val length: Int) extends IndexedSeq[T] with IndexedSeqLike[T, RingBuffer[T]] {
+  var offset = 0
+  def apply(idx: Int) = buf(idx + offset % length)
+  def +=(el: T)
+}
+*/
+
+class RingBuffer[T](buf: Array[T], val length: Int) {
+  private var offset = 0
+
+  private def idxFor(idx: Int): Int =
+    idx + offset % length
+
+  def apply(idx: Int) = buf(idxFor(idx))
+
+  def +=(el: T) = {
+    buf(idxFor(0)) = el
+    offset += 1
+  }
+}
+
+object FindTheMin extends CmdlineInput with Logging {
+  /*
   def slidingWindow(s: Seq[Long], k: Int) = {
     //(s takeRight k zipWithIndex) groupBy (_._2)
 
@@ -13,15 +38,35 @@ object FindTheMin extends CmdlineInput {
     //(s takeRight k groupBy identity mapValues (_ length) keySet) toPriorityQueue  
     ()
   }
+  */
 
-  def toLongs(str: String) = str split ' ' map (Long parseLong _)
+  def fillArray(a: Long, b: Long, c: Long, r: Long, k: Int) = {
+    def next(state: Long) = b * state + c % r
+    val ret = ArrayBuffer.empty[Long]
+    ret.sizeHint(k)
+    var state = a
+    var i: Int = 0
+    while (i < k) {
+      ret += state
+      state = next(state)
+      i += 1
+    }
+    val ret2 = ArrayBuffer.iterate(a, k)(next) 
+    assert (ret == ret2)
+    ret2
+  }
+
+  def split(str: String) = str split ' '
+  def toLongs(str: String) = split(str) map (Long parseLong _)
+  def toInts(str: String) = split(str) map (Integer parseInt _)
   def main(args: Array[String]) {
     val (lines, t) = getInputAndCount(args) //t is at most 50.
-    processInput(lines.grouped(2).toSeq, t) { linePair =>
-      val nk = toLongs(linePair(0))
+    processInput(lines.tail.grouped(2).toSeq, t) { linePair =>
+      val nk = toInts(linePair(0))
       val (n, k) = (nk(0), nk(1))
       val rngParams = toLongs(linePair(1))
       val (a, b, c, r) = (rngParams(0), rngParams(1), rngParams(2), rngParams(3))
+      val initialArray = fillArray(a, b, c, r, k)
       ""
     }
   }
