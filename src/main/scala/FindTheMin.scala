eaf00cb8cbfe610aae16b8feb862a631aa99b40b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-01-28 04:53:36 +0100

    Add solution idea

--- src/main/scala/FindTheMin.scala
+++ src/main/scala/FindTheMin.scala
@@ -2,6 +2,18 @@ import java.lang.Long
 import language.postfixOps
 
 object FindTheMin extends CmdlineInput {
+  def slidingWindow(s: Seq[Long], k: Int) = {
+    //(s takeRight k zipWithIndex) groupBy (_._2)
+
+    //We need to incrementally maintain this when new elements are added at the end of s,
+    //so that the sliding window moves.
+    ((s takeRight k groupBy identity mapValues (_ length)) keySet)
+    //What we still don't get is a quick way to find the first non-zero element.
+    //That requires a priority queue - in pseudocode:
+    //(s takeRight k groupBy identity mapValues (_ length) keySet) toPriorityQueue  
+    ()
+  }
+
   def toLongs(str: String) = str split ' ' map (Long parseLong _)
   def main(args: Array[String]) {
     val (lines, t) = getInputAndCount(args) //t is at most 50.
