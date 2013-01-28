296227dad43493b095e09e6652287350c7e03196
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-01-28 03:48:58 +0100

    Instrument code to time parsing
    This is a very minimal timing infrastructure, but it's OK for my goal
    of measuring startup performance, and not steady-state one.

--- src/main/scala/BalancedSmileys.scala
+++ src/main/scala/BalancedSmileys.scala
@@ -2,6 +2,17 @@ import language.postfixOps
 import com.codecommit.gll._
 //import com.codecommit.util
 
+trait Timing {
+  this: Logging =>
+  def timed[T](toTime: => T): T = {
+    val start = System.nanoTime()
+    val res = toTime
+    val end = System.nanoTime
+    println(s"${(end - start)/(1000 * 1000)} ms needed")
+    res
+  }
+}
+
 trait BalancedSmileys extends RegexParsers {
   override final val skipWhitespace = false
   /*def validChar: Parser[Elem] = acceptIf {
@@ -37,26 +48,28 @@ trait BalancedSmileys extends RegexParsers {
     msg ~ msg ^^ (_ + _)*/
 }
 
-object BalancedSmileysDriver extends BalancedSmileys with CmdlineInput with Logging {
+object BalancedSmileysDriver extends BalancedSmileys with CmdlineInput with Logging with Timing {
   val debugFailures = false
   def main(args: Array[String]) {
     val (lines, t) = getInputAndCount(args) //t is at most 50.
     processInput(lines, t) { line =>
-      if (msg(line) collectFirst {
-        case x @ Success(y, rest) if rest.isEmpty => ()
-      } isEmpty) {
-        if (debugFailures) {
-          for (i <- msg(line)) {
-            i match {
-              case failure : Failure =>
-                println(failure)
-              case _ =>
+      timed {
+        if (msg(line) collectFirst {
+          case x @ Success(y, rest) if rest.isEmpty => ()
+        } isEmpty) {
+          if (debugFailures) {
+            for (i <- msg(line)) {
+              i match {
+                case failure : Failure =>
+                  println(failure)
+                case _ =>
+              }
             }
           }
+          "NO"
+        } else {
+          "YES"
         }
-        "NO"
-      } else {
-        "YES"
       }
     }
   }
