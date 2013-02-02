7852c412d7058f57cecc6f3894e127dcd8cb23d5
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-02-02 18:47:20 +0100

    Solution skeleton

---
+++ src/main/scala/round1/Prob1.scala
@@ -0,0 +1,11 @@
+package round1
+import util._
+
+object Prob1 extends Logging with CmdlineInput {
+  def main(args: Array[String]) {
+    val (lines, m) = getInputAndCount(args)
+      processInput(lines, m) { line =>
+      ???
+    }
+  }
+}
